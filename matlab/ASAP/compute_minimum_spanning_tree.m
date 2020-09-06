97ebae8931fbbd631862008b4db7b7588c57a038
Aliaksei Mikhailiuk <aliaksei.mikhailiuk@gmail.com>
2020-09-06 16:27:35 +0100

    Update compute_minimum_spanning_tree.m

--- matlab/ASAP/compute_minimum_spanning_tree.m
+++ matlab/ASAP/compute_minimum_spanning_tree.m
@@ -1,4 +1,6 @@
 function [pairs_to_compare] = compute_minimum_spanning_tree(inf_mat)
+    % Asap allows for batch mode, for which we return N-1 pairs of conditions for comparisons
+    % forming the minimum spanning tree of the 1/information_gain matrix
     inf_mat = inf_mat+inf_mat';
     inf_mat = 1./inf_mat;
     inf_mat(inf_mat<0) = Inf;
