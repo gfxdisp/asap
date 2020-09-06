f80e18247de2aa5c1b62cb299c0ef2986298135a
Aliaksei Mikhailiuk <aliaksei.mikhailiuk@gmail.com>
2020-09-06 16:26:21 +0100

    comments

--- matlab/ASAP/unroll_mat.m
+++ matlab/ASAP/unroll_mat.m
@@ -1,4 +1,6 @@
 function [G] = unroll_mat(M)
+% turn matrix m into an array with dimensionality (numb_comparisons, 2), where in row kk 
+% tupple [3,4] means that in the kth comparison condition 3 was chosen over condition 4
 N = size(M,1);
 G = [];
     for ii=1:N
