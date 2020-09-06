2e687a317a5798fb032724b5f10bd1796b357633
Aliaksei Mikhailiuk <aliaksei.mikhailiuk@gmail.com>
2020-09-06 16:25:47 +0100

    comments

--- matlab/ASAP/run_asap.m
+++ matlab/ASAP/run_asap.m
@@ -5,9 +5,12 @@ function [pairs_to_compare] = run_asap(M,mode)
     assert(strcmp(mode, 'mst') || strcmp(mode,'seq'),'Please pass valid mode, either seq or mst.')
     
     
-    N = size(M,1);    
+    N = size(M,1);
+    % turn matrix m into an array with dimensionality (numb_comparisons, 2), where in row kk 
+    % tupple [3,4] means that in the kth comparison condition 3 was chosen over condition 4
     G = unroll_mat(M);
     
+    # Set starting parameters for the TrueSkill
     init_data.n_iter = 4;
     init_data.Ms=zeros(N,1);
     init_data.Vs=0.5*ones(N,1);
@@ -17,6 +20,7 @@ function [pairs_to_compare] = run_asap(M,mode)
     init_data.G = G;
     init_data.prob_cmps = ones(N);
     
+    # Compute the information gain matrix and return a single pair maximizing information gain
     [inf_mat,pairs_to_compare]=compute_information_gain_mat(N,init_data);
     if strcmp(mode,'mst')
         pairs_to_compare = compute_minimum_spanning_tree(inf_mat);
